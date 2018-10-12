a1cf468f58afd8add0df10193f10fd18c9537095
Rohit Sharma <rohitsharma290619@gmail.com>
2018-10-12 10:45:13 -0500

    Initial neuralnet implementation

--- Assignments/3-NeuralNetwork/src/neuralnet.py
+++ Assignments/3-NeuralNetwork/src/neuralnet.py
@@ -2,6 +2,7 @@ from scipy.io import arff
 import numpy as np
 import pandas as pd
 import sys
+import math
 
 
 def import_data(dataset_arff_path):
@@ -12,6 +13,28 @@ def import_data(dataset_arff_path):
     """
     data, meta = arff.loadarff(dataset_arff_path)
     return pd.DataFrame(data), meta
+
+
+def sigmoid(input):
+    return 1.0 / (1.0 + math.exp(-input))
+
+
+def neuralnet_learn(dataset, input_layer_dim, hidden_layer_dim, output_layer_dim, num_folds, learning_rate, num_epochs):
+    # np.random.seed(100)
+    num_instances = dataset.shape[0]
+
+    weights_layer_1 = np.random.uniform(-1.0, 1.0, (input_layer_dim, hidden_layer_dim))
+    bias_layer_1 = np.random.uniform(-1.0, 1.0, (num_instances, hidden_layer_dim))
+    weights_layer_2 = np.random.uniform(-1.0, 1.0, (hidden_layer_dim, output_layer_dim))
+    bias_layer_2 = np.random.uniform(-1.0, 1.0, (num_instances, output_layer_dim))
+
+    z_layer_1 = dataset.iloc[:, :-1].dot(weights_layer_1) + bias_layer_1
+    activate_layer_1 = z_layer_1.applymap(lambda val: sigmoid(val))
+
+    z_layer_2 = activate_layer_1.dot(weights_layer_2) + bias_layer_2
+    activate_layer_2 = z_layer_2.applymap(lambda val: sigmoid(val))
+
+    print activate_layer_2
 
 
 def main():
@@ -30,6 +53,12 @@ def main():
     num_epochs = int(sys.argv[4])
 
     dataset, metadata = import_data(training_data_file_path)
+
+    input_layer_dim = dataset.shape[1] - 1      # Excluded 1 for class attribute
+    hidden_layer_dim = input_layer_dim
+    output_layer_dim = 1
+
+    neuralnet_learn(dataset, input_layer_dim, hidden_layer_dim, output_layer_dim, num_folds, learning_rate, num_epochs)
 
 
 # Calling the main function
