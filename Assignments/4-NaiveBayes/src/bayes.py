42dc96a452e6e90a4b4506295581cbc87e480106
Rohit <rohit.sharma@wisc.edu>
2018-10-31 23:43:54 -0500

    Commandline args

--- Assignments/4-NaiveBayes/src/bayes.py
+++ Assignments/4-NaiveBayes/src/bayes.py
@@ -1,7 +1,15 @@
+import sys
 
 
 def main():
-    pass
+    if len(sys.argv) != 4:
+        print 'Usage: bayes <train-set-file> <test-set-file> <n|t>'
+        sys.exit(1)
+
+    training_data_file_path = 'dataset/' + sys.argv[1]
+    testing_data_file_path = 'dataset/' + sys.argv[2]
+
+    naive_bayes = True if sys.argv[3] == 'n' else False
 
 
 if __name__ == '__main__':
