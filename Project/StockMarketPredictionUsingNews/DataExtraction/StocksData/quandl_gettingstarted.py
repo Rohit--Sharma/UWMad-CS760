9090a396966d17529e8fdd733b169f225ee7e7b5
Rohit <rohit.sharma@wisc.edu>
2018-11-27 22:18:50 -0600

    Added Dow Jones Industrial Average dataset

--- Project/StockMarketPredictionUsingNews/DataExtraction/StocksData/quandl_gettingstarted.py
+++ Project/StockMarketPredictionUsingNews/DataExtraction/StocksData/quandl_gettingstarted.py
@@ -7,5 +7,14 @@ quandl.ApiConfig.api_key = "HC3WjNA-trqxLy42S5Nm"
 
 data = quandl.get("EIA/PET_RWTC_D", returns="numpy")
 data = quandl.get("EOD/MSFT", returns="numpy")
+data = quandl.get_table("SHARADAR/SF2", paginate=True)
+print 'Data:'
+print data
 
-data_table = quandl.get_table("MER/F1", paginate=True)
+dow_jones = quandl.get('BCB/UDJIAD1', returns="numpy")
+print 'Dow Jones:'
+print dow_jones
+
+# data_table = quandl.get_table("MER/F1", paginate=True)
+# print 'Data table:'
+# print data_table
